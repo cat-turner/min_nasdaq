d89a7567826e59347f711f8be15be37e245329b9
cathleen turner <turner.cathleen@gmail.com>
2018-03-25 01:35:11 +0000

    notes before changes

--- public/angular-app/app.js
+++ public/angular-app/app.js
@@ -14,24 +14,31 @@ function config($routeProvider) {
       controller: StockInfoController,
       controllerAs: 'vm'
     })
-    .when('/login', {
-      templateUrl:'/angular-app/login/login.html',
-      controller:LoginController,
-      controllerAs:'vm'
-    })
+
   
 }
 
+    // .when('/login', {
+    //   templateUrl:'/angular-app/login/login.html',
+    //   controller:LoginController,
+    //   controllerAs:'vm'
+    // })
+
 // TODO - 
-// add authentication
-// i. add html code to form
-// ii.try out form. Should be able to submit data
-// iii. change user login to post method, and test route
+// log in form - front end
+// i . add login page
+// ii. add register page
+// iii. check if works - should return jwt token, log that in console
+
+// investigate how jsonwebtoken works... again.
+// authentication - more back end
+// i. auth. verifies web tokens passed in through request
+
 
 //WANTS
 //clean up - at the end
 // determine if items are being posted multiple times
 // refactor stock search + stock all
 // figure out why search by Name isn't as responsive. Is query taking
-//too long?
+//too long? Doesn't seem to work most of the times
 // refactor - put repeat methods like jsonResponse somehwere. Maybe a lib
